d4e1937ba08ed21e26c0c34c0a21a529ff27306d
CauZy-Goes <cauafariasdev@gmail.com>
2024-05-27 12:02:55 -0300

    feat: add DeveloperTask Entity. Close #5

---
+++ src/main/java/ucsal/codehub/developer_task/model/entities/DeveloperTask.java
@@ -0,0 +1,37 @@
+package ucsal.codehub.developer_task.model.entities;
+
+import ucsal.codehub.developer_task.model.enums.SkillLevel;
+
+public class DeveloperTask extends AssignedTask<Developer>{
+
+    private SkillLevel skillLevel;
+
+    public DeveloperTask(String name, Developer person, SkillLevel skillLevel) {
+        super(name, person);
+        this.skillLevel = skillLevel;
+    }
+
+    public DeveloperTask(String name, Developer person, Integer id, SkillLevel skillLevel) {
+        super(name, person, id);
+        this.skillLevel = skillLevel;
+    }
+
+    public SkillLevel getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(SkillLevel skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "DeveloperTask{" +
+                ", name='" + name + '\'' +
+                ", id=" + id +
+                ", person=" + person +
+                ", status=" + status +
+                "skillLevel=" + skillLevel +
+                '}';
+    }
+}
